fbad19f6bea98bc24c75c45566b421b26279ffff
saru2017 <saru@ist.osaka-u.ac.jp>
2018-11-10 23:53:29 +0900

    update for database based recomendation.

--- weather.py
+++ weather.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python
+#!/data/apps/anaconda/python
 # coding: utf-8 
 
 import requests
 import sys
 import io
-import bs4
 import lxml.html
+import csv
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
@@ -14,31 +14,21 @@ sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
 def get_weather():
     url = "https://weather.yahoo.co.jp/weather/jp/27/6200.html"
     r = requests.get(url)
-#    print(vars(r))
-#    html = r.text
-#    start = html.find('<div class="forecastCity">')
-#    end = html.find('</div>', start)
-#    html = html[start : end]
-#    print(html)
-#    soup = bs4.BeautifulSoup(r.text, "html.parser")
-#    items = soup.select(".forecastCity")
-#    print(items)
     dom = lxml.html.fromstring(r.text)
-#    items = dom.xpath("//div[@class='forecastCity']/table/tr/td/div/p")
     items = dom.xpath("//p[@class='pict']/img")
-#    print(items[0].attrib['alt'])
-#    print(items[1].attrib['alt'])
     weather = items[0].attrib['alt']
 
     items = dom.xpath("//ul[@class='temp']/li/em")
-
-#    print(items[0].text)
-#    print(items[1].text)
     max_t = int(items[0].text)
     min_t = int(items[1].text)
     
     return (weather, max_t, min_t)
 
+
+def create_db():
+    f = open("/data/config/log.csv", "r", encoding="utf-8")
+    lst = list(csv.reader(f))
+    return lst
     
 
 if __name__ == "__main__":
@@ -47,6 +37,25 @@ if __name__ == "__main__":
     if max_t > 28:
         print("ポロシャツ")
 
+    if min_t > 20:
+        print("半袖T")
+    elif min_t > 10:
+        print("長袖T")
+    else:
+        print("長袖T×2")
+        
+    items = create_db()
+        
     print("チノパン")
 
+    d = 10000
+    ret = ""
+
+    for item in items:
+        l = (max_t - int(item[0]))**2 + (min_t - int(item[1]))**2
+        if l < d:
+            ret = item
+            d = l
+
+    print(ret)
 
